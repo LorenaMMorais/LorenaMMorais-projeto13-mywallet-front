8d53bc25148d0c140dec0eebb81a28edc1cbad36
Lorena <lorenamorais@alunos.utfpr.edu.br>
2022-07-04 10:59:45 -0300

    feat: Add Login page

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import ReactDOM from 'react-dom';
+import App from './componentes/App';
+
+import './css/reset.css';
+import './css/style.css';
+
+ReactDOM.render(<App />, document.querySelector('.root'));
