872d8673ca1cdf7cc7a52a1cba20e8bda41df9ce
Lorena <lorenamorais@alunos.utfpr.edu.br>
2022-07-04 13:48:57 -0300

    feat: Add layout and routes Inputs and Outputs pages

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './Login';
 import Register from './Register'
 import Transactions from './Transactions';
+import Inputs from './Inputs';
+import Outputs from './Outputs';
+
 
 export default function App() {
     return (
@@ -11,6 +14,8 @@ export default function App() {
                 <Route path='/sign-in' element={<Login />} />
                 <Route path='/sign-up' element={<Register />} />
                 <Route path='/transactions' element={<Transactions />} />
+                <Route path='/transactions/inputs' element={<Inputs />} />
+                <Route path='/transactions/outputs' element={<Outputs />} />
             </Routes>
         </BrowserRouter>
     );
